43377eea948a2d4f2d86a84136101459ffbef287
rlaeogus0911 <rlaeogus0911@nate.com>
2020-06-24 14:25:59 +0900

    학생관련 클래스 B

---
+++ src/controller/StudentB.java
@@ -0,0 +1,18 @@
+package controller;
+
+public class StudentB {
+	private int no;
+
+	public StudentB(int no) {
+		super();
+		this.no = no;
+	}
+
+	public StudentB() {
+		super();
+		// TODO Auto-generated constructor stub
+	}
+
+	
+
+}
